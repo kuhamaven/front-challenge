927f5ee56089083e817e834b52b8bcc77f76e401
Kuha <khalil.stessens@ing.austral.edu.ar>
2024-07-01 14:27:35 -0300

    Fixes for vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ReducedDigimon } from '../types/Digimon';
 
-const API_URL = 'https://digi-api.com/api/v1';
+const API_URL = '/api/v1';
 
 export const fetchCharacters = async (page: number): Promise<ReducedDigimon[]> => {
     const response = await axios.get(`${API_URL}/digimon/?name=&page=${page}`);
